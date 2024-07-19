db58cac10f2bcfcf97cdcc0ae36769ddd307e37a
Eli Selkin <eli.j.selkin@gmail.com>
2024-07-19 14:40:09 -0700

    Initial Commit

---
+++ simple-pwd-generatorTests/simple_pwd_generatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  simple_pwd_generatorTests.swift
+//  simple-pwd-generatorTests
+//
+//  Created by Eli Selkin on 7/19/24.
+//
+
+import Testing
+@testable import simple_pwd_generator
+
+struct simple_pwd_generatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
